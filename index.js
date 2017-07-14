f48a73362fbecfcf63e997928a2883611f27897e
Stephen Whitmore <sww@eight45.net>
2017-07-14 12:24:59 -0700

    Include .notFound on errors produced by unfound chunks

--- index.js
+++ index.js
@@ -35,7 +35,11 @@ Storage.prototype.get = function (index, opts, cb) {
   if (typeof opts === 'function') return this.get(index, null, opts)
   if (this.closed) return nextTick(cb, new Error('Storage is closed'))
   var buf = this.chunks[index]
-  if (!buf) return nextTick(cb, new Error('Chunk not found'))
+  if (!buf) {
+    var err = new Error('Chunk not found')
+    err.notFound = true
+    return nextTick(cb, err)
+  }
   if (!opts) return nextTick(cb, null, buf)
   var offset = opts.offset || 0
   var len = opts.length || (buf.length - offset)
